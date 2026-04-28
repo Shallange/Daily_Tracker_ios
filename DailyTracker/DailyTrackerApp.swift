2df0f8825d87b8d1a9de71e03f1ccdfd26217a13
Shallange <jimmy.kroneld@hotmail.com>
2026-04-28 10:51:28 +0200

    Initial commit: Project setup

---
+++ DailyTracker/DailyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTrackerApp.swift
+//  DailyTracker
+//
+//  Created by Jimmy kroneld on 2026-04-28.
+//
+
+import SwiftUI
+
+@main
+struct DailyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
