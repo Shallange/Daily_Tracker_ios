e52a567ef13634edae954232827536b7c21347b0
Shallange <jimmy.kroneld@hotmail.com>
2026-05-04 13:13:34 +0200

    Add tab navigation for Tracker and Progress

---
+++ DailyTracker/Views/MainTabView.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabView.swift
+//  DailyTracker
+//
+//  Created by Jimmy kroneld on 2026-05-04.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            HabitListView()
+                .tabItem {
+                    Label("Tracker", systemImage: "checklist")
+                }
+
+            StatsDashboardView()
+                .tabItem {
+                    Label("Progress", systemImage: "chart.bar")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
---
+++ DailyTracker/Views/StatsDashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  DailyTracker
+//
+//  Created by Jimmy kroneld on 2026-05-04.
+//
+
+import SwiftUI
+
+struct StatsDashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatsDashboardView()
+}
